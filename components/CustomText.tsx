e3ee492b03897a615b5210fad2e404060b43b77b
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-16 16:35:41 -0300

    text estilizada pra ter uma fonte diferentezinha

---
+++ components/CustomText.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text } from 'react-native';
+
+interface Atributos
+{
+    children?: string | string[];
+}
+
+export default function CustomText({children}: Atributos)
+{
+    const estilo = StyleSheet.create(
+    {
+        Text:
+        {
+            fontFamily: 'system-ui',
+            fontSize: 20,
+            textAlign: 'center'
+        }
+    });
+
+    return (
+        <Text style={estilo.Text}>
+            {children}
+        </Text>
+    );
+}
