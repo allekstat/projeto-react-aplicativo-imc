1a3f198680b24912893f7a643e4500e0fadc516b
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-16 16:36:00 -0300

    depois muda o button pra ficar melhor, esse teal não tá bom

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,18 +1,33 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
 interface Atributos
 {
     texto?: string;
     click?: () => void;
 }
+
 export default function CustomButton({texto, click}: Atributos)
 {
-    const estilo = StyleSheet.create({ CustomButton:
+    const estilo = StyleSheet.create(
     {
-        backgroundColor: 'red'
-    }});
+        Text:
+        {
+            color: 'white'
+        },
+        TouchableOpacity:
+        {
+            backgroundColor: 'teal',
+            padding: 8,
+            margin: 8,
+            marginHorizontal: 'auto',
+            borderRadius: 8,
+            
+        }
+    });
+    
     return (
-        <TouchableOpacity onPress={click} style={estilo.CustomButton}>
-            <Text>{texto}</Text>
+        <TouchableOpacity onPress={click} style={estilo.TouchableOpacity}>
+            <Text style={estilo.Text}>{texto}</Text>
         </TouchableOpacity>
     );
 }
