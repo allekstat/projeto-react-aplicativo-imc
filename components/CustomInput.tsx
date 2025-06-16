ff9600bca7ebe482b187a4587daaaeb275e6049a
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-16 16:37:21 -0300

    eu testei e a função de chamada tá funcionando

---
+++ components/CustomInput.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, TextInput } from 'react-native';
+
+interface Atributos
+{
+    children?: string;
+    onChangeText?: (texto: string) => void;
+}
+
+export default function CustomInput({onChangeText, children}: Atributos)
+{
+    const estilo = StyleSheet.create(
+    {
+        TextInput:
+        {
+            backgroundColor: 'white',
+            marginHorizontal: 'auto',
+            margin: 8,
+            padding: 8,
+            borderRadius: 8
+        }
+    });
+
+    return (
+        <TextInput onChangeText={onChangeText} style={estilo.TextInput}>
+            {children}
+        </TextInput>
+    );
+}
