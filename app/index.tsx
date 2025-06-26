f34e9414cc207da66d0be3ce7c9f8bebb9520250
Alexsander <alexsander.santana2@etec.sp.gov.br>
2025-06-26 19:33:11 -0300

    calculo
    coloquei o cálculo na página inicial pq tava dando problema e não tínhamos tanto tempo pra resolve-los

--- app/index.tsx
+++ app/index.tsx
@@ -4,29 +4,41 @@ import CustomInput from '@/components/CustomInput';
 import CustomText from '@/components/CustomText';
 import CustomView from '@/components/CustomView';
 import { useRouter } from 'expo-router';
-import React from "react";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import React = require('react');
+import { Alert } from 'react-native';
 
 export default function AppIndex()
 {
-    const router = useRouter();
+    // const router = useRouter();
     const [nome, setNome] = useState('');
-
-    function entrar_no_sistema()
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [imc, setImc] = useState('');
+    const calcular = () => 
     {
-        if (true)
+        if (!peso || isNaN(Number(peso)))
         {
-            sessionStorage.setItem('nome', nome)
-            router.push('/calculos');
+            Alert.alert('Erro', 'Peso Inválido', [{text: 'OK', }]);
+            return;
         }
+        else if (!altura || isNaN(Number(altura)))
+        {
+            Alert.alert('Erro', 'Altura Inválida', [{text: 'OK', }]);
+            return;
+        }
+        setImc(String(10000 * Number(peso) / Number(altura) / Number(altura)));
     }
-
     return (
         <CustomView>
             <CustomForm>
-                <CustomText>Digite o Seu Nome</CustomText>
-                <CustomInput onChangeText={setNome}></CustomInput>
-                <CustomButton texto='Entrar no Sistema' click={entrar_no_sistema}></CustomButton>
+                <CustomText>Bem Vindo(a){nome}{'\n\n'}</CustomText>
+                <CustomText>Digite seu Peso (quilos)</CustomText>
+                <CustomInput onChangeText={x => {setPeso(x); setImc('')}}></CustomInput>
+                <CustomText>Digite sua Altura (centímetros)</CustomText>
+                <CustomInput onChangeText={x => {setAltura(x); setImc('')}}></CustomInput>
+                <CustomButton texto='Calcular IMC' click={calcular}></CustomButton>
+                <CustomText>{imc ? `Seu IMC é ${imc}` : ''}</CustomText>
             </CustomForm>
         </CustomView>
     )
